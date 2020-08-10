2a22220c928533ced2109176edd3900cfa923d0a
yakobus <thiagotnon@gmail.com>
2020-08-10 13:58:53 -0300

    Ajustes no FrontEnd

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,34 +1,54 @@
 import React from 'react';
+import api from '../../services/api';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem () {
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string,
+}
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection () {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
            <header>
-             <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGHkFhj9fb_5g/profile-displayphoto-shrink_100_100/0?e=1602720000&v=beta&t=_0mKCBweRkWVY7_QI1uSpyZ90dvkFSM3G2W1ZhrPagw" alt="Thiago Nascimento"/>
+             <img src={teacher.avatar} alt={teacher.name}/>
              <div>
-               <strong>Thiago Nascimento</strong>
-               <span>Química</span>
+               <strong>{teacher.name}</strong>
+               <span>{teacher.subject}</span>
              </div>
            </header>
-           <p>
-            Entusiasta das melhores tecnologias de química avançada.
-            <br/><br/>
-            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-           </p>
+           <p>{teacher.bio}</p>
 
            <footer>
              <p>
                Preço/hora
-               <strong>R$ 100,00</strong>
+               <strong>R$ {teacher.cost}</strong>
              </p>
-             <button type="button">
+             <a
+                target="_blank"
+                onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}
+              >
                <img src={whatsappIcon} alt="Whatsapp"/>
                Entrar em contato
-             </button>
+             </a>
            </footer>
         </article>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,34 +1,92 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
-
+import searchIcon from '../../assets/images/icons/search.svg';
 import './styles.css';
-import TeacherItem from '../../components/TeacherItem';
+import api from '../../services/api';
+
 
 function TeacherList () {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeekday] = useState('');
+  const [time, setTime] = useState('');
+
+  async function searchTeachers (event: FormEvent) {
+    event.preventDefault();
+
+    const response = await api.get('classes', {
+      params: {
+        subject,
+        week_day,
+        time
+      }
+    });
+
+    setTeachers(response.data);
+    
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
+        <form id="search-teachers" onSubmit={searchTeachers}>
+        <Select 
+          name="subject" 
+          label="Matéria"
+          value={subject}
+          onChange={event => setSubject(event.target.value)}
+          options={[
+            { value: 'Artes', label: 'Artes' },
+            { value: 'Biologia', label: 'Biologia' },
+            { value: 'Física', label: 'Física' },
+            { value: 'Geografia', label: 'Geografia' },
+            { value: 'História', label: 'História' },
+            { value: 'Literatura', label: 'Literatura' },
+            { value: 'Matemática', label: 'Matemática' },
+            { value: 'Português', label: 'Português' },
+            { value: 'Química', label: 'Química' },
+            { value: 'Redação', label: 'Redação' },
+          ]}
+        />
+         
+         <Select 
+          name="week_day" 
+          label="Dia da semana"
+          value={week_day}
+          onChange={event => setWeekday(event.target.value)}
+          options={[
+            { value: '0', label: 'Domingo' },
+            { value: '1', label: 'Segunda-feira' },
+            { value: '2', label: 'Terça-feira' },
+            { value: '3', label: 'Quarta-feira' },
+            { value: '4', label: 'Quinta-feira' },
+            { value: '5', label: 'Sexta-feira' },
+            { value: '6', label: 'Sábado' },
+          ]}
+        />
+         <Input 
+          type="time" 
+          name="time" 
+          label="Hora"
+          value={time}
+          onChange={event => setTime(event.target.value)}
+        />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+        <button type="submit">
+          <img src={searchIcon} alt="Buscar"/>
+        </button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />;
+        })}
       </main>
     </div>
   );
